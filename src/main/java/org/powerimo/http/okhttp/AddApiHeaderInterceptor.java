035adf9e0dbcda92fea63a474c9cfcb09ccdb16a
Voronin Oleg <voronin@andewil.com>
2024-07-21 19:56:00 +0200

    AddApiHeaderInterceptor

---
+++ src/main/java/org/powerimo/http/okhttp/AddApiHeaderInterceptor.java
@@ -0,0 +1,27 @@
+package org.powerimo.http.okhttp;
+
+import lombok.Getter;
+import lombok.Setter;
+import okhttp3.Interceptor;
+import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+@Getter
+@Setter
+public class AddApiHeaderInterceptor implements Interceptor {
+    private String apiKeyHeader = "x-api-key";
+    private String apiKey;
+
+    @NotNull
+    @Override
+    public Response intercept(@NotNull Chain chain) throws IOException {
+        var request = chain.request();
+        var authorizedRequest = request.newBuilder()
+                .addHeader(apiKeyHeader, apiKey)
+                .build();
+
+        return chain.proceed(authorizedRequest);
+    }
+}
