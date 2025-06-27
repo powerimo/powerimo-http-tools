2e4c27ebcb419b8a180eeb549563ccc241f3e392
andewil <voronin@andewil.com>
2025-06-27 22:28:29 +0200

    Make details public

--- src/main/java/org/powerimo/http/exceptions/AccessTokenObtainFailed.java
+++ src/main/java/org/powerimo/http/exceptions/AccessTokenObtainFailed.java
@@ -1,15 +1,20 @@
 package org.powerimo.http.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class AccessTokenObtainFailed extends RuntimeException {
     private final String details;
+    private static final String DETAILED_MESSAGE = "%s. Details: %s";
 
     public AccessTokenObtainFailed(String message, String details, Throwable cause) {
-        super(message, cause);
+        super(String.format(DETAILED_MESSAGE, message, details), cause);
         this.details = details;
     }
 
     public AccessTokenObtainFailed(String message, String details) {
-        super(message);
+        super(String.format(DETAILED_MESSAGE, message, details));
         this.details = details;
     }
+
 }
